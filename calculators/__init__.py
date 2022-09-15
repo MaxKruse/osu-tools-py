f2561633bc628d9cdd4538060695c149deaadb4d
Maximilian Kruse <maxkruse1996@gmail.com>
2022-09-15 22:00:49 +0200

    add streamin calculator for command

--- calculators/__init__.py
+++ calculators/__init__.py
@@ -1,7 +1,8 @@
-
 from calculators.xexxar_v1.XexxarCalc import XexxarPerformanceCalculator
+from calculators.StreaminCalc.StreaminCalc import StreaminCalculator
 
 
 PP_CALCULATORS = {
-    "xexxar_v1": XexxarPerformanceCalculator
+    "xexxar_v1": XexxarPerformanceCalculator,
+    "streamin_v1": StreaminCalculator
 }
